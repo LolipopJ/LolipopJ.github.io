43337a55a7e6b9a121b4de33aba02cf1ac9b0e53
LolipopJ <42314340+LolipopJ@users.noreply.github.com>
2024-05-19 16:38:41 +0800

    feat(theme): update source files of hexo-theme-arhcer

--- themes/archer/src/js/main.js
+++ themes/archer/src/js/main.js
@@ -1,11 +1,10 @@
 import Package from '../../package.json'
 
 import init from './init'
-import { scroll } from './scroll'
+import initTheme from './theme'
+import initScroll from './scroll'
+import initImage from './image'
 import './initSidebar'
-import { initMobile } from './mobile'
-import InitSidebarLink from './tag'
-import fancybox from './fancybox'
 
 // print console info
 const logStyle =
@@ -15,32 +14,14 @@ console.info(`%c 🏷 Version: ${Package.version} `, logStyle)
 console.info(`%c 📅 Version date: ${Package.versionDate} `, logStyle)
 console.info(`%c 📦 ${Package.homepage} `, logStyle)
 
-// print loaded info
-window.addEventListener('load', function (event) {
-  console.log('All resources are loaded!')
-})
-
-// init site base
+// site base
 init()
 
-// init mobile
-initMobile()
+// theme
+initTheme()
+
+// init image lazy-loading & fancybox
+initImage()
 
 // scroll event
-scroll()
-
-// init sidebar link
-const metas = new InitSidebarLink()
-metas.addTab({
-  metaName: 'tags',
-  labelsContainer: '.sidebar-tags-name',
-  postsContainer: '.sidebar-tags-list',
-})
-metas.addTab({
-  metaName: 'categories',
-  labelsContainer: '.sidebar-categories-name',
-  postsContainer: '.sidebar-categories-list',
-})
-
-// fancybox
-fancybox()
+initScroll()
